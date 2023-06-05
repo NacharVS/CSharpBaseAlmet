aa3016e4bd7cd256b77bd91e5ebb6d797a8087fe
nacharvs <r3dinc@yandex.ru>
2023-06-05 11:38:38 +0300

    if-else Ex2

--- CSharpBaseAlmet/Program.cs
+++ CSharpBaseAlmet/Program.cs
@@ -1,24 +1,34 @@
 ﻿Console.WriteLine("Start");
 
 int a = int.Parse(Console.ReadLine());
-int b = int.Parse(Console.ReadLine());
-int c = int.Parse(Console.ReadLine());
 
-if(a > b && a > c && c > b)   // & && | ||
+int sot = 0;
+int des = 0;
+int ed = 0;
+
+
+if (a > 99 && a < 1000)
 {
-    Console.WriteLine(a);
-}
-else
-{
-    if (b > c)
+
+    ed = a % 10;
+    sot = a / 100;
+    des = a % 100 / 10;
+
+    if (a % 2 == 0)
     {
-        Console.WriteLine(b);
+        Console.WriteLine($"Число четное. Сумма - {ed + sot + des} пам-пам");
     }
     else
     {
-        Console.WriteLine(c);
+        Console.WriteLine($"Число нечетное. Произведение - {ed * sot * des} пам-пам");
     }
 }
+else
+{
+    Console.WriteLine("Number incorrect");
+}
+
+
 
 Console.WriteLine("End");
 
