d162e73b0bc35d7f4a48c9e5b28442c9b19f6148
nacharvs <r3dinc@yandex.ru>
2023-06-05 15:25:06 +0300

    arrays max element ex1

--- CSharpBaseAlmet/Program.cs
+++ CSharpBaseAlmet/Program.cs
@@ -1,22 +1,27 @@
 ﻿Console.WriteLine("Start");
 int x = int.Parse(Console.ReadLine());
 int[] numbers = new int[x]; //length = 10 maxIndex = length - 1;
-
+int max = 0;
+int index = 0;
 Random random = new Random();
 
 for (int i = 0; i < numbers.Length; i++)
 {
-    numbers[i] = random.Next(-5, 5);
+    numbers[i] = random.Next(10, 100);
+    Console.Write(numbers[i] + " ");
 }
 
-Console.WriteLine("Generation complete");
-
-foreach (int item in numbers)
+for (int i = 0; i < numbers.Length; i++)
 {
-    if (item == 0)
-        break;
-    Console.WriteLine(item);
+    if (numbers[i] > max)
+    {
+        max = numbers[i];
+        index = i;
+    }
 }
+Console.WriteLine($"max - {max}, index of max - {index}");
+
+
 
 
 // 0 1 1 2 3 5 8 13 21 34 ....
