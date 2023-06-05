7b9c9eab630dae9f182c42fed102d4c4e2c3928a
nacharvs <r3dinc@yandex.ru>
2023-06-05 10:28:06 +0300

    Base operations

--- CSharpBaseAlmet/Program.cs
+++ CSharpBaseAlmet/Program.cs
@@ -1,2 +1,15 @@
 ﻿
-Console.WriteLine("Hello, World!");
+
+int a = int.Parse(Console.ReadLine());
+
+int sot = 0;
+int des = 0;
+int ed = 0;
+
+ed = a % 10;
+sot = a / 100;
+des = a % 100 / 10;
+
+
+Console.WriteLine(ed + sot + des);
+
