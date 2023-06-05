251f1880f82f663758d0056ce9ffd7b7315cb466
nacharvs <r3dinc@yandex.ru>
2023-06-05 16:09:43 +0300

    array sotrting

--- CSharpBaseAlmet/Program.cs
+++ CSharpBaseAlmet/Program.cs
@@ -1,7 +1,9 @@
 ﻿Console.WriteLine("Start");
-//int x = int.Parse(Console.ReadLine());
-int[] arrayOne = new int[12]; //length = 10 maxIndex = length - 1;
-int[] arrayTwo = new int[6];
+
+int[] arrayOne = new int[12];
+
+int buffer = 0;
+int checks = 0;
 Random random = new Random();
 
 for (int i = 0; i < arrayOne.Length; i++)
@@ -10,46 +12,31 @@ for (int i = 0; i < arrayOne.Length; i++)
     Console.Write(arrayOne[i] + " ");
 }
 Console.WriteLine();
-for (int i = 0; i < arrayTwo.Length; i++)
-{
-    arrayTwo[i] = random.Next(10, 100);
-    Console.Write(arrayTwo[i] + " ");
-}
-Console.WriteLine();
 
-if(arrayOne.Length > arrayTwo.Length)
+for (int i = 0; i < arrayOne.Length; i++)
 {
-    for (int i = 0; i < arrayTwo.Length; i++)
+    for (int j = 0; j < arrayOne.Length - 1 - i; j++)
     {
-        arrayOne[i] = arrayOne[i] + arrayTwo[i];
+        if(arrayOne[j] > arrayOne[j + 1])
+        {
+            buffer = arrayOne[j];
+            arrayOne[j] = arrayOne[j + 1];
+            arrayOne[j + 1] = buffer;
+        }
+        checks++;
     }
+    Console.WriteLine();
     foreach (var item in arrayOne)
     {
-        Console.Write(item + " ");
+        Console.Write(" " + item);
     }
 }
-else
-{
-    for (int i = 0; i < arrayOne.Length; i++)
-    {
-        arrayTwo[i] = arrayOne[i] + arrayTwo[i];       
-    }
-    foreach (var item in arrayTwo)
-    {
-        Console.Write(item + " ");
-    }
-}
-
-
-
-
-
-
-
-
-
-
-// 0 1 1 2 3 5 8 13 21 34 ....
 Console.WriteLine();
-Console.WriteLine("End");
+Console.WriteLine("Sorted:");
+foreach (var item in arrayOne)
+{
+    Console.Write(" " + item);
+}
+Console.WriteLine();
+Console.WriteLine(checks);
 
