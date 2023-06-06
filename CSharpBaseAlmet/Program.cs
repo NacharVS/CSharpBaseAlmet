4efcd64c88c5cf048ec5405f6eb62c8495cd19a5
nacharvs <r3dinc@yandex.ru>
2023-06-06 11:39:30 +0300

    ex2

--- CSharpBaseAlmet/Program.cs
+++ CSharpBaseAlmet/Program.cs
@@ -1,28 +1,71 @@
 ﻿using CSharpBaseAlmet;
+//Дана матрица заполненная рандомными значениями.
+//Необходимо отсортировать номера строк в порядке
+//возрасстания суммы в строке
 
-List<Unit> unitList = new List<Unit>();
+int[,] array = new int[10, 10];
+NumSum[] nums = new NumSum[10];
 Random random = new Random();
-Unit unitBuffer;
+NumSum buffer = new NumSum();
 
-for (int i = 0; i < 10; i++)
+for (int i = 0; i < nums.Length; i++)
 {
-    unitBuffer = new Unit("Worker", random.Next(30, 91), 
-        random.Next(9, 19), random.Next(0, 8));
-    unitList.Add(unitBuffer);
+    nums[i] = new NumSum();
 }
 
-foreach (var item in unitList)
+for (int i = 0; i < array.GetLength(0); i++)
 {
-    Console.WriteLine($"{item.Type} - {item.Health} - {item.Speed} - {item.Defense}");
+    nums[i].Number = i + 1;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = random.Next(10, 99);
+        Console.Write($"{array[i, j]} ");
+        nums[i].Sum += array[i, j];
+
+    }
+
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+foreach (var item in nums)
+{
+    Console.WriteLine($"{item.Number} - {item.Sum}");
+}
+
+for (int i = 0; i < nums.Length; i++)
+{
+    for (int j = 0; j < nums.Length - 1 - i; j++)
+    {
+        if (nums[j].Sum > nums[j + 1].Sum)
+        {
+            //bufSum = nums[j].Sum;
+            //bufNum = nums[j].Number;
+            //nums[j].Sum = nums[j + 1].Sum;
+            //nums[j].Number = nums[j + 1].Number;
+            //nums[j + 1].Sum = bufSum;
+            //nums[j + 1].Number = bufNum;
+            buffer = nums[j];
+            nums[j] = nums[j + 1];
+            nums[j + 1] = buffer;
+
+        }
+
+    }
+    Console.WriteLine();
 }
 Console.WriteLine();
-foreach (var item in unitList)
+
+foreach (var item in nums)
 {
-    if(item.Health > 60 && item.Speed > 12)
-    {
-        Console.WriteLine($"{item.Type} - {item.Health} - {item.Speed} - {item.Defense}");
-    }
+    Console.WriteLine($"{item.Number} - {item.Sum}");
 }
+
+
+
+
+
 
 
 
