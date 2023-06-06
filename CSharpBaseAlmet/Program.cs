4b4aab329d6a552e386b66157381abdddfabe70a
nacharvs <r3dinc@yandex.ru>
2023-06-06 12:01:01 +0300

    unit methods

--- CSharpBaseAlmet/Program.cs
+++ CSharpBaseAlmet/Program.cs
@@ -1,66 +1,16 @@
 ﻿using CSharpBaseAlmet;
-//Дана матрица заполненная рандомными значениями.
-//Необходимо отсортировать номера строк в порядке
-//возрасстания суммы в строке
 
-int[,] array = new int[10, 10];
-NumSum[] nums = new NumSum[10];
-Random random = new Random();
-NumSum buffer = new NumSum();
-
-for (int i = 0; i < nums.Length; i++)
-{
-    nums[i] = new NumSum();
-}
-
-for (int i = 0; i < array.GetLength(0); i++)
-{
-    nums[i].Number = i + 1;
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-        array[i, j] = random.Next(10, 99);
-        Console.Write($"{array[i, j]} ");
-        nums[i].Sum += array[i, j];
-
-    }
-
-    Console.WriteLine();
-}
-
-Console.WriteLine();
-
-foreach (var item in nums)
-{
-    Console.WriteLine($"{item.Number} - {item.Sum}");
-}
-
-for (int i = 0; i < nums.Length; i++)
-{
-    for (int j = 0; j < nums.Length - 1 - i; j++)
-    {
-        if (nums[j].Sum > nums[j + 1].Sum)
-        {
-            //bufSum = nums[j].Sum;
-            //bufNum = nums[j].Number;
-            //nums[j].Sum = nums[j + 1].Sum;
-            //nums[j].Number = nums[j + 1].Number;
-            //nums[j + 1].Sum = bufSum;
-            //nums[j + 1].Number = bufNum;
-            buffer = nums[j];
-            nums[j] = nums[j + 1];
-            nums[j + 1] = buffer;
-
-        }
-
-    }
-    Console.WriteLine();
-}
-Console.WriteLine();
-
-foreach (var item in nums)
-{
-    Console.WriteLine($"{item.Number} - {item.Sum}");
-}
+Unit.CreateUnit();
+Unit peasant1 = new Unit("Worker", 30, 9, 0);
+Unit footman = new Unit("Warrior", 60, 10, 2);
+peasant1.GetDamage(10);
+peasant1.ShowInfo();
+peasant1.GetDamage(10);
+peasant1.ShowInfo();
+peasant1.GetDamage(10);
+peasant1.ShowInfo();
+peasant1.GetDamage(10);
+peasant1.ShowInfo();
 
 
 
--- CSharpBaseAlmet/Unit.cs
+++ CSharpBaseAlmet/Unit.cs
@@ -15,6 +15,29 @@
         public int Speed { get; set; }
         public int Defense { get; set; }
 
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Type - {Type} Health - {Health}");
+        }
+
+        public static void CreateUnit()
+        {
+            Console.WriteLine("Unit has created");
+        }
+
+        public void GetDamage(int damage)
+        {
+            Console.WriteLine($"{Type} got {damage} of damage;");
+            if (Health <= damage)
+            {
+                Console.WriteLine("Destroyed");
+                Health = 0;
+            }
+            else
+            {
+                Health -= damage;
+            }            
+        }
 
     }
 }
