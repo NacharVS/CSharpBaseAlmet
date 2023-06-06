0264375143cbe8149cd1bc2ce48aadc95696c50a
nacharvs <r3dinc@yandex.ru>
2023-06-06 10:06:34 +0300

    methods overloads

--- CSharpBaseAlmet/Program.cs
+++ CSharpBaseAlmet/Program.cs
@@ -1,5 +1,4 @@
-﻿GetBinaryArray();
-
+﻿
 
 
 
@@ -30,5 +29,45 @@ static int[,] GetBinaryArray()
     }
     return array;
 }
+
+static void ShowCustomArray(string name, int rows, int cols, int min, int max)
+{
+    int[,] array = new int[rows, cols];
+    Console.WriteLine($"Array '{name}' ");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(min, max);
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+static void ArrayGenegation(int[] numbers, int min, int max)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(min, max);
+        Console.Write(numbers[i] + " ");
+    }
+}
+
+static void Summ(params double[] ints)
+{
+    int sum = 0;
+    foreach (int i in ints)
+    {
+        sum += i;
+    }
+    Console.WriteLine(sum);
+}
+
+static void ShowInConsole(string s)
+{
+    Console.WriteLine(s);
+}
+
+
 
 
