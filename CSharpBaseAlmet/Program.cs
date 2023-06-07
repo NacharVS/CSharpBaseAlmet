4f35a184ee1d49ee710ae3903e9b991d12b1af3d
corptn\kalmykovim <KalmykovIM@tatneft.tatar>
2023-06-07 15:14:02 +0300

    Interfaces start1

--- CSharpBaseAlmet/Program.cs
+++ CSharpBaseAlmet/Program.cs
@@ -16,34 +16,31 @@ void Method3()
     Console.WriteLine("Move");
 }
 
-Peasant peasant = new Peasant();
-
-
-
+//Peasant peasant = new Peasant();
 //peasant.moving = Method1;
 //peasant.Move();
 //peasant.moving = Method2;
 //peasant.Move();
 //peasant.moving = Method3;
 //peasant.Move();
-IronSword sword = new IronSword();
-BronzeMace mace = new BronzeMace();
-StoneAxe axe = new StoneAxe();
-Footman soldier1 = new Footman("Warrior", 60, 11, 2, 2, axe);
-Footman soldier2 = new Footman("Warrior", 60, 11, 2, 2, mace);
-Smith smith1 = new Smith();
+//IronSword sword = new IronSword();
+//BronzeMace mace = new BronzeMace();
+//StoneAxe axe = new StoneAxe();
+//Footman soldier1 = new Footman("Warrior", 60, 11, 2, 2, axe);
+//Footman soldier2 = new Footman("Warrior", 60, 11, 2, 2, mace);
+//Smith smith1 = new Smith();
 
-Archer archer = new Archer("Ranger", 40, 12, 15, 1);
-Healer healer = new Healer();
+//Archer archer = new Archer("Ranger", 40, 12, 15, 1);
+//Healer healer = new Healer();
 
-peasant.HealthChangedEvent += Method1;
+//peasant.HealthChangedEvent += Method1;
 //peasant.HealthChangedEvent += Method2;
 //peasant.HealthChangedEvent += Method3;
 
-soldier1.CurrentWeapon.DurabilityChangedEvent += axe.ShowDurability;
+//soldier1.CurrentWeapon.DurabilityChangedEvent += axe.ShowDurability;
 
-soldier1.InflictDamage(peasant);
-soldier1.InflictDamage(peasant);
+//soldier1.InflictDamage(peasant);
+//soldier1.InflictDamage(peasant);
 
 //soldier1.WeaponAttack(soldier2); 
 //soldier2.WeaponAttack(soldier1); 
@@ -76,7 +73,17 @@ soldier1.InflictDamage(peasant);
 //soldier2.ShowBaseInfo();
 
 
+IRangeWeapon weapon1 = new Cannon();
+IRangeWeapon weapon2 = new Bow();
 
+weapon1.Shoot();
+weapon1.ShowInfo();
+weapon2.Shoot();
+weapon2.ShowInfo();
+weapon1.Shoot();
+weapon1.ShowInfo();
+weapon2.Shoot();
+weapon2.ShowInfo();
 
 
 
--- CSharpBaseAlmet/Weapons/IRangeWeapon.cs
+++ CSharpBaseAlmet/Weapons/IRangeWeapon.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CSharpBaseAlmet.Weapons
+﻿namespace CSharpBaseAlmet.Weapons
 {
-    internal interface IWeapon
+    interface IRangeWeapon
     {
+        public int RangeDamage { get; set; }
+
+        public int Range { get; set; }
+
+        public void Shoot();
+        public void ShowInfo();
     }
 }
